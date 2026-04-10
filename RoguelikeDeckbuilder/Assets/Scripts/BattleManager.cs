818d66c52aec579473346d271fc4a53d0f256583
Daehyeon Kim <daehyeon.dev@gmail.com>
2026-04-10 10:47:30 +0900

    feat: Add Turn
    -배틀 매니저에 enum 형식으로 Turn 추가
    -Player랑 Enemy 공격이 각자의 턴에만 진행하도록 추가
    -Turn 변경을 확인할 수 있도록 Log 및 UI로 출력

--- RoguelikeDeckbuilder/Assets/Scripts/BattleManager.cs
+++ RoguelikeDeckbuilder/Assets/Scripts/BattleManager.cs
@@ -1,21 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class BattleManager : MonoBehaviour
 {
+	//enum
+	public enum BattleTurnState
+	{
+		PlayerTurn,
+		EnemyTurn,
+		Busy,
+		BattleEnd
+	}
+
 	//SerializeField
 	[Header("BattleUnitBase")]
 	[SerializeField] private BattleUnitBase _playerUnit;
     [SerializeField] private UnitAttack _playerUnitAttack;
     [SerializeField] private BattleUnitBase _enemyUnit;
 	[SerializeField] private UnitAttack _enemyUnitAttack;
+	[Header("UI")]
+	[SerializeField] private TMP_Text _textMeshPro;
 	//private variables
-
+	private BattleTurnState _currentTurn;
 	//private components
 
 	//public property
-
+	public BattleTurnState CurrentTurn
+	{
+		get { return _currentTurn; }
+		set { _currentTurn = value;
+			Debug.Log($"{_currentTurn}");
+			_textMeshPro.text = _currentTurn.ToString();
+		}
+	}
 	//Unity Lifecycle
 	private void Awake()
 	{
@@ -30,7 +49,7 @@ public class BattleManager : MonoBehaviour
 
     private void Start()
     {
-
+		CurrentTurn = BattleTurnState.PlayerTurn;
     }
 
     private void Update()
@@ -43,14 +62,26 @@ public class BattleManager : MonoBehaviour
 	//region Private Methods
 	private void PlayerAttack()
 	{
-		if(!_playerUnit.IsDead && !_enemyUnit.IsDead)
+		if (CurrentTurn != BattleTurnState.PlayerTurn)
+			return;
+		if (!_playerUnit.IsDead && !_enemyUnit.IsDead)
+		{
+			CurrentTurn = BattleTurnState.Busy;
 			_playerUnitAttack.Attack(_enemyUnit);
+			CurrentTurn = BattleTurnState.EnemyTurn;
+		}			
 	}
 
 	private void EnemyAttack()
 	{
+		if (CurrentTurn != BattleTurnState.EnemyTurn)
+			return;
         if (!_playerUnit.IsDead && !_enemyUnit.IsDead)
+		{
+			CurrentTurn = BattleTurnState.Busy;
             _enemyUnitAttack.Attack(_playerUnit);
+			CurrentTurn = BattleTurnState.PlayerTurn;
+        }
 	}
 
 	//region Gizmos
